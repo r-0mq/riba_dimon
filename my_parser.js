1137969f1a581616061465d80b5a1df3f264d19c
r0m-q <romadelphi1996@gmail.com>
2017-03-14 23:58:39 +0200

    Processing of logs

--- my_parser.js
+++ my_parser.js
@@ -1,13 +1,33 @@
-// Загрузка модуля fs (файловая система)
+// Загрузка модуля fs
 var fs = require('fs');
 
 // Считывание содержимого файла в память
 fs.readFile('example_log.txt', function (err, logData) {
 
-    // Если произошла ошибка, то мы генерируем исключение,
-    // и работа приложения завершается
+    // Если произошла ошибка, то генерируем исключение,
+    // и работа приложения завершится.
     if (err) throw err;
 
-    // logData имеет тип Buffer, переводим в string
+    // logData имеет тип Buffer, переводим в строку
     var text = logData.toString();
+
+    var results = {};
+
+    // Разбивка файла по строкам
+    var lines = text.split('\n');
+
+    lines.forEach(function(line) {
+        var parts = line.split(' ');
+        var letter = parts[1];
+        var count = parseInt(parts[2]);
+
+        if(!results[letter]) {
+            results[letter] = 0;
+        }
+
+        results[letter] += parseInt(count);
+    });
+
+    console.log(results);
+    // { A: 2, B: 14, C: 6 }
 });
